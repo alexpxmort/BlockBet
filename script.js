c1f0cc3f4a45d363f55f2a2d570b9d509c24d71a
Alex Pereira de Oliveira <37446253+alexpxmort@users.noreply.github.com>
2024-01-14 23:30:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-chrome.webRequest.onBeforeRequest.addListener(
-    function(details) {
-        console.log("I am going to block:", details.url)
-        chrome.tabs.update(null, {url: "https://api.whatsapp.com/send?phone=+5511995716942&text=Ola+preciso+de+ajuda%2Cacabei+de+acessar+um+site+de+apostas+%21"});
+function blockSite(details, tabsUpdate) {
+    console.log("I am going to block:", details.url);
+    tabsUpdate(null, {url: "https://api.whatsapp.com/send?phone=+5511995716942&text=Ola+preciso+de+ajuda%2Cacabei+de+acessar+um+site+de+apostas+%21"});
+    return {cancel: true};
+}
 
-        return {cancel: true}
-    },
-    {urls: blocked_sites_v2},
-    ["blocking"]
-)
+if (typeof chrome !== "undefined" && chrome.webRequest) {
+    chrome.webRequest.onBeforeRequest.addListener(
+        details => blockSite(details, chrome.tabs.update),
+        {urls: blocked_sites_v2},
+        ["blocking"]
+    );
+} else if (typeof browser !== "undefined" && browser.webRequest) {
+    browser.webRequest.onBeforeRequest.addListener(
+        details => blockSite(details, browser.tabs.update),
+        {urls: blocked_sites_v2},
+        ["blocking"]
+    );
+}
