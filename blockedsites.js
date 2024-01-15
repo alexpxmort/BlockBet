b2e0620e1de1a3603ac7574599701f59e3605648
Alex Pereira de Oliveira <37446253+alexpxmort@users.noreply.github.com>
2024-01-14 23:37:13 -0300

    Update blockedsites.js

--- blockedsites.js
+++ blockedsites.js
@@ -1,4 +1,11 @@
 var blocked_sites_v2 = [
   "https://pixbet.com/",
-  "https://m.esportesdasorte.com/"
+  "https://m.esportesdasorte.com/",
+  "https://br.betano.com",
+  "https://promo.sportingbet.com",
+  "https://www.bet365.com" 
+  "https://lp.superbet.com",
+  "https://promotions.betfair.com",
+  "https://blaze-4.com/pt/"
+  
 ]
