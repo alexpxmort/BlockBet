560316de5c68c1ad8aa47c8ca2e73f6e6dfc2580
Alex <lexpdigi@gmail.com>
2024-01-14 22:45:45 -0300

    add zap

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
         console.log("I am going to block:", details.url)
+        chrome.tabs.update(null, {url: "https://api.whatsapp.com/send?phone=+5511995716942&text=Ola+preciso+de+ajuda%2Cacabei+de+acessar+um+site+de+apostas+%21"});
+
         return {cancel: true}
     },
     {urls: blocked_sites_v2},
